03700155ea3b41af71469e6f107af1d14a77a53d
Mikel Severson <mkseve@gmail.com>
2016-07-20 21:18:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Unity Node Project
+
+Currently the goal of this project is to experiment with Unity.
+
+Node.js will be the backend.
+
+I have no knowledge with unity, so I have no scope planned for this project. Multiplayer is a necessity. 
---
+++ server/server.js
@@ -0,0 +1,12 @@
+var io = require('socket.io')(process.env.PORT || 3000);
+
+console.log('server started');
+
+io.on('connection', (socket) => {
+  console.log('client spawned');
+  soket.broadcast.emit('spawn');
+
+  socket.on('move', () => {
+      console.log('client moved');
+  });
+});
