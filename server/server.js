a923ecb9828d8484935e5ad49e9178a7fdd3df66
Mikel Severson <mkseve@gmail.com>
2016-07-22 20:21:52 -0500

    feat: Add player count and player spawn

--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@ var io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server started');
 
+var playerCount = 0;
+
 io.on('connection', (socket) => {
-  console.log('client spawned');
+  console.log('client connected, broadcasting spawn');
+
   socket.broadcast.emit('spawn');
+  playerCount++;
+
+  for(i=0; i < playerCount; i++) {
+    socket.emit('spawn');
+    console.log('sending spawn to new player')
+  }
 
   socket.on('move', () => {
       console.log('client moved');
