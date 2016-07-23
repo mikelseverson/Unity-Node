42255c0368eedb2fb4a26dc9428749b1a4ded98e
Mikel Severson <mkseve@gmail.com>
2016-07-23 11:25:39 -0500

    feat: Add player movement

---
+++ UnityNode/Assets/NavigatePosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigatePosition : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void NavigateTo(Vector3 position) {
+		agent.SetDestination(position);
+	}
+}
---
+++ UnityNode/Assets/ScreenClicker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenClicker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire2")) {
+				Clicked();
+			}
+	}
+
+	void Clicked() {
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit hit = new RaycastHit ();
+
+		if(Physics.Raycast(ray, out hit)) {
+			Debug.Log(hit.collider.gameObject.name);
+
+			var clickMove = hit.collider.gameObject.GetComponent<ClickMove>();
+			clickMove.OnClick(hit.point);
+		}
+	}
+}
