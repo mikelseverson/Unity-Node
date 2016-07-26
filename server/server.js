8b88074ef2de9d0e2969ed2c07e8447ede8c916b
Mikel Severson <mkseve@gmail.com>
2016-07-25 21:52:18 -0500

    feat(server): Spawn preexisting players on load

--- server/server.js
+++ server/server.js
@@ -7,22 +7,28 @@ const shortid = require('shortid');
 console.log('server started');
 
 var playerCount = 0;
+var players = [];
 
 io.on('connection', (socket) => {
-  let thisClientId = shortid.generate();
+  let thisPlayerId = shortid.generate();
+  players.push(thisPlayerId);
+  console.log('client connected, broadcasting spawn, id: ', thisPlayerId);
 
-  console.log('client connected, broadcasting spawn, id: ', thisClientId);
-
-  socket.broadcast.emit('spawn', {id: thisClientId});
+  socket.broadcast.emit('spawn', {id: thisPlayerId});
   playerCount++;
 
+  players.forEach(playerId => {
+    if(playerId == thisPlayerId) return;
+    socket.emit('spawn', {id: playerId});
+  })
+
   for(let i=0; i < playerCount; i++) {
-    console.log('sending spawn to new player')
+    console.log('spawning to new player')
     socket.emit('spawn');
   };
 
   socket.on('move', (data) => {
-      data.id = thisClientId;
+      data.id = thisPlayerId;
       console.log('client moved', JSON.stringify(data));
       socket.broadcast.emit('move', data);
   });
